93def651e979c7cde43a13a8d0b4c96acab76e34
Peter Ling <pling01@yahoo.com>
2021-02-06 07:39:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,33 +4,33 @@ function basicTeenager(age) {
   }
 
 }
+//
+// function teenager(age) {
+//   if (age > 12 && age < 20){
+//     return `"You are a teenager!"`;
+//   } else {
+//     return `"You are not a teenager"`;
+//   }
+//
+// }
 
-function teenager(age) {
-  if (age > 12 && age < 20){
-    return `"You are a teenager!"`;
-  } else {
-    return `"You are not a teenager"`;
-  }
+// function ageChecker(age) {
+//   if (age < 13){
+//     return "You are a kid"
+//   } else if (age > 19){
+//     return "You are a grownup";
+//   } else {
+//     return "You are a teenager!";
+//     }
+//
+//
+// }
 
-}
-
-function ageChecker(age) {
-  if (age < 13){
-    return "You are a kid"
-  } else if (age > 19){
-    return "You are a grownup";
-  } else {
-    return "You are a teenager!";
-    }
-
-
-}
-
-function ternaryTeenager(age) {
-  return age <= 19 && age >= 13 : true : false;
-
-}
-
-function switchAge(age) {
-
-}
+// function ternaryTeenager(age) {
+//   return age <= 19 && age >= 13 : true : false;
+//
+// }
+//
+// function switchAge(age) {
+//
+// }
